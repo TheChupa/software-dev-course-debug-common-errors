5fdd2e3a180a5158c9d5f1108061d465235d0412
Michael <200~pregnant-mutt-cape@duck.com>
2025-03-14 08:47:58 -0500

    fixed and noted errors in code

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,23 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
-
-// What’s Wrong?
-
+console.log("Welcome to the bootcamp");
+  
+  // What’s Wrong?
+  //There is a a syntax error. Missing end quote, parenthsis, and semicolon at end of line.
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+// "eight" should be the num 8. Changed to a number.
 
 
 // Program C (Logic Error)
@@ -51,12 +51,14 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
-console.log(isPrime(7)); // Expected true but gets false
+console.log(isPrime(11)); // Expected true but gets false
 
 // What’s Wrong?
+
+// True and false statements are backwards. Should be return true, number is isPrime.
